feb3c7ca9e15a6d1ca7ddc7265cb3d4adc5a5a93
godrikw <peresvet_it@mail.ru>
2021-10-04 21:16:28 +0300

    2

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Slider} from "./Components/Slider";
+import './index.css';
+
+
+
+function App() {
+
+  return (
+    <div className={"main"}>
+    <Slider/>
+    </div>
+  );
+}
+
+export default App;
