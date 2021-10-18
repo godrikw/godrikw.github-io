032d2936dbecaa1849fbd292275d35c462d0b035
godrikw <peresvet_it@mail.ru>
2021-10-18 05:40:56 +0300

    loader

--- src/Components/Partfolio/index.jsx
+++ src/Components/Partfolio/index.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Offer} from "./Offer";
 import {MainContent} from "./MainContent";
 import {Project} from "./Project";
 import {Footer} from "./Footer";
+import './index.css'
+
 
 export const Portfolio = (props) => {
+
+    const [isSpinner, setIsSpinner] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => setIsSpinner(false), 2500)
+    }, [])
+
     return (
         <div >
+            {isSpinner && <div className='preloader'>
+                <div className="loader">
+                    <div className="loader-inner">
+                        <div className="loader-line-wrap">
+                            <div className="loader-line"/>
+                        </div>
+                        <div className="loader-line-wrap">
+                            <div className="loader-line"/>
+                        </div>
+                        <div className="loader-line-wrap">
+                            <div className="loader-line"/>
+                        </div>
+                        <div className="loader-line-wrap">
+                            <div className="loader-line"/>
+                        </div>
+                        <div className="loader-line-wrap">
+                            <div className="loader-line"/>
+                        </div>
+                    </div>
+                </div>
+            </div>}
+            {!isSpinner &&
             <div id={"test1"}>
                 <Offer state={props.state} states={props.states}/>
                 <MainContent/>
                 <Project/>
                 <Footer/>
-            </div>
+            </div>}
 
 
         </div>
